228c12a389d0f261c639d67c421a4801529dfc7d
Ayman Aboelazm <ayman.m.aboelazm@gmail.com>
2020-07-23 10:45:33 +0200

    massive updates

--- Streamia/Controllers/SeriesesController.cs
+++ Streamia/Controllers/SeriesesController.cs
@@ -36,27 +36,6 @@ namespace Streamia.Controllers
             return View();
         }
 
-        //[HttpPost]
-        //public IActionResult AddEpisode(Series series)
-        //{
-        //    Dictionary<int, List<Episode>> model = new Dictionary<int, List<Episode>>();
-        //    for (int i = 0; i < series.SeasonData.Length; i++)
-        //    {
-        //        List<Episode> episodes = new List<Episode>();
-        //        int season = i + 1;
-        //        for (int y = 0; y < series.SeasonData[i]; y++)
-        //        {
-        //            int episode = y + 1;
-        //            episodes.Add(new Episode { 
-        //                Season = season,
-        //                Number = episode
-        //            });
-        //        }
-        //        model.Add(season, episodes);
-        //    }
-        //    return PartialView("~/Views/Serieses/Episode.cshtml", model);
-        //}
-
         private async Task PrepareViewBag()
         {
             ViewBag.Categories = await categoryRepository.Search(m => m.CategoryType == CategoryType.SERIES);
