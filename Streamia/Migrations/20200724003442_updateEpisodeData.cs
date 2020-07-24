bc5e11cfa6a5bd6b0dac0483fbefc4dd174584ad
Ayman Aboelazm <ayman.m.aboelazm@gmail.com>
2020-07-24 02:36:35 +0200

    massive updates

---
+++ Streamia/Migrations/20200724003442_updateEpisodeData.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Streamia.Migrations
+{
+    public partial class updateEpisodeData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Director",
+                table: "Episode",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Episode",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Overview",
+                table: "Episode",
+                nullable: true);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Rating",
+                table: "Episode",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReleaseDate",
+                table: "Episode",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Director",
+                table: "Episode");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Episode");
+
+            migrationBuilder.DropColumn(
+                name: "Overview",
+                table: "Episode");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Episode");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Episode");
+        }
+    }
+}
--- Streamia/Models/Episode.cs
+++ Streamia/Models/Episode.cs
@@ -13,6 +13,11 @@ namespace Streamia.Models
         public int Number { get; set; }
         public int Season { get; set; }
         public string Source { get; set; }
+        public string Name { get; set; }
+        public string Overview { get; set; }
+        public string Director { get; set; }
+        public string ReleaseDate { get; set; }
+        public float Rating { get; set; }
         public Series Series { get; set; }
     }
 }
