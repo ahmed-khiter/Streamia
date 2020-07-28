2494c3f1cf94a6154e8d061ef18bf10b28f5bf81
Ayman Aboelazm <ayman.m.aboelazm@gmail.com>
2020-07-28 03:42:11 +0200

    updates

--- Streamia/Models/Stream.cs
+++ Streamia/Models/Stream.cs
@@ -16,28 +16,23 @@ namespace Streamia.Models
         [Required]
         public string Source { get; set; }
 
-        public string Notes { get; set; }
-
         [Display(Name = "Generate PTS")]
         public bool GeneratePts { get; set; } = true;
 
-        [Display(Name = "Stream All")]
-        public bool StreamAll { get; set; }
+        [Display(Name = "Enable RTMP")]
+        public bool EnableRtmp { get; set; }
 
-        [Display(Name = "Allow RTMP")]
-        public bool AllowRtmp { get; set; }
+        [Display(Name = "Enable Recording")]
+        public bool EnableRecording { get; set; }
 
-        [Display(Name = "Recording")]
-        public bool AllowRecording { get; set; }
-
-        [Display(Name = "Direct Source")]
-        public bool DirectSource { get; set; }
+        [Display(Name = "Stream Directly")]
+        public bool StreamDirectly { get; set; }
 
         [Display(Name = "Enigma SID")]
         public string EnigmaSID { get; set; }
 
-        [Display(Name = "Minutes to Delay")]
-        public int MinuteDelay { get; set; } = 0;
+        [Display(Name = "Delay in minutes")]
+        public int Delay { get; set; } = 0;
 
         public ICollection<StreamServer> StreamServers { get; set; }
         public ICollection<BouquetStream> BouquetStreams { get; set; }
