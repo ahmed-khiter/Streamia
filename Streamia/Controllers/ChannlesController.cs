3e4eeafd07498a1ec5fc4b0e9c33951cb4784281
Ayman Aboelazm <ayman.m.aboelazm@gmail.com>
2020-07-29 03:46:52 +0200

    updates

---
+++ Streamia/Controllers/ChannlesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Streamia.Models;
+using Streamia.Models.Enums;
+using Streamia.Models.Interfaces;
+
+namespace Streamia.Controllers
+{
+    public class ChannlesController : Controller
+    {
+        private readonly IRepository<Channel> channelRepository;
+        private readonly IRepository<Bouquet> bouquetRepository;
+        private readonly IRepository<Server> serverRepository;
+
+        public ChannlesController(
+            IRepository<Channel> channelRepository,
+            IRepository<Bouquet> bouquetRepository,
+            IRepository<Server> serverRepository
+        )
+        {
+            this.channelRepository = channelRepository;
+            this.bouquetRepository = bouquetRepository;
+            this.serverRepository = serverRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Channel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Manage));
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return RedirectToAction(nameof(Manage));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Manage()
+        {
+            var model = 0;
+            return View(model);
+        }
+
+        private async Task PrepareViewBag()
+        {
+            ViewBag.Servers = await serverRepository.Search(m => m.ServerState == ServerState.ONLINE);
+            ViewBag.Bouquets = await bouquetRepository.GetAll();
+        }
+    }
+}
--- Streamia/Models/Channel.cs
+++ Streamia/Models/Channel.cs
@@ -8,12 +8,5 @@ namespace Streamia.Models
 {
     public class Channel : BaseEntity
     {
-        [Required]
-        public string Name { get; set; }
-        public string Path { get; set; }
-        public string Logo { get; set; }
-        public string Note { get; set; }
-        public bool StartChannel { get; set; } = false;
-        public Category Category { get; set; }
     }
 }
