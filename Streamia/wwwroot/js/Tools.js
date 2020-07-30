cc8756b9edb6028236d5d05d10f73c7f50cb7ef0
Ayman Aboelazm <ayman.m.aboelazm@gmail.com>
2020-07-31 00:12:34 +0200

    updates

--- Streamia/wwwroot/js/Tools.js
+++ Streamia/wwwroot/js/Tools.js
@@ -1,11 +1,18 @@
 ﻿class Tools {
-    isURL(url) {
-        var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    urlMonitor() {
+        let pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
             '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
             '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
             '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
             '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
             '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
-        return pattern.test(url);
+
+        document.querySelectorAll('.tools-url-monitor').forEach(tum => tum.addEventListener('keyup', function () {
+            if (!!pattern.test(this.value)) {
+                document.querySelectorAll('.tools-url-action').forEach(tua => tua.style.display = 'block');
+            } else {
+                document.querySelectorAll('.tools-url-action').forEach(tua => tua.style.display = 'none');
+            }
+        }));
     }
 }
