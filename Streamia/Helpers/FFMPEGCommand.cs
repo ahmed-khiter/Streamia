5a21e3a2173a8a0a9a51dc44514b0e4616d4a3b5
Ayman Aboelazm <ayman.m.aboelazm@gmail.com>
2020-08-04 05:47:39 +0200

    updates

--- Streamia/Helpers/FFMPEGCommand.cs
+++ Streamia/Helpers/FFMPEGCommand.cs
@@ -11,14 +11,70 @@ namespace Streamia.Helpers
     {
         public static string GenerateTranscodeParams(Transcode transcodeProfile)
         {
-            StringBuilder command = new StringBuilder(" -y -nostdin -hide_banner ");
-            command.Append($" -c:v {transcodeProfile.VideoCodec ?? "copy"}");
-            command.Append($" -profile:v {transcodeProfile.VideoProfile ?? "main"}");
+            StringBuilder command = new StringBuilder(" -y -nostdin -hide_banner INPUT_SRC");
+            // 1920x1080
+            command.Append(" -vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease");
             command.Append($" -c:a {transcodeProfile.AudioCodec ?? "copy"}");
-            if (!transcodeProfile.Preset.Equals("default"))
-            {
-                command.Append($" -preset {transcodeProfile.Preset}");
-            }
+            command.Append(transcodeProfile.AudioSampleRate_1080 > 0 ? $" -ar {transcodeProfile.AudioSampleRate_720}" : string.Empty);
+            command.Append($" -c:v {transcodeProfile.VideoCodec ?? "copy"}");
+            command.Append(!transcodeProfile.Preset.Equals("default") ? $" -preset {transcodeProfile.Preset}" : string.Empty);
+            command.Append($" -profile:v {transcodeProfile.VideoProfile ?? "main"}");
+            command.Append($" -crf {(transcodeProfile.CRF > 0 ? transcodeProfile.CRF.ToString() : "20")}");
+            command.Append(" -hls_time 4");
+            command.Append(" -hls_playlist_type LIST_TYPE");
+            command.Append(transcodeProfile.VideoBitrate_1080 > 0 ? $" -b:v {transcodeProfile.VideoBitrate_1080}k" : string.Empty);
+            command.Append(transcodeProfile.MaxBitrate_1080 > 0 ? $" -maxrate {transcodeProfile.MaxBitrate_1080}k" : string.Empty);
+            command.Append(transcodeProfile.BufferSize_1080 > 0 ? $" -bufsize {transcodeProfile.BufferSize_1080}k" : string.Empty);
+            command.Append(transcodeProfile.AudioBitrate_1080 > 0 ? $" -b:a {transcodeProfile.AudioBitrate_1080}k" : string.Empty);
+            command.Append(" -hls_segment_filename OUTPUT_SRC");
+
+            // 1280x720
+            command.Append(" -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease");
+            command.Append($" -c:a {transcodeProfile.AudioCodec ?? "copy"}");
+            command.Append(transcodeProfile.AudioSampleRate_720 > 0 ? $" -ar {transcodeProfile.AudioSampleRate_720}" : string.Empty);
+            command.Append($" -c:v {transcodeProfile.VideoCodec ?? "copy"}");
+            command.Append(!transcodeProfile.Preset.Equals("default") ? $" -preset {transcodeProfile.Preset}" : string.Empty);
+            command.Append($" -profile:v {transcodeProfile.VideoProfile ?? "main"}");
+            command.Append($" -crf {(transcodeProfile.CRF > 0 ? transcodeProfile.CRF.ToString() : "20")}");
+            command.Append(" -hls_time 4");
+            command.Append(" -hls_playlist_type LIST_TYPE");
+            command.Append(transcodeProfile.VideoBitrate_720 > 0 ? $" -b:v {transcodeProfile.VideoBitrate_720}k" : string.Empty);
+            command.Append(transcodeProfile.MaxBitrate_720 > 0 ? $" -maxrate {transcodeProfile.MaxBitrate_720}k" : string.Empty);
+            command.Append(transcodeProfile.BufferSize_720 > 0 ? $" -bufsize {transcodeProfile.BufferSize_720}k" : string.Empty);
+            command.Append(transcodeProfile.AudioBitrate_720 > 0 ? $" -b:a {transcodeProfile.AudioBitrate_720}k" : string.Empty);
+            command.Append(" -hls_segment_filename OUTPUT_SRC");
+
+            // 842x480
+            command.Append(" -vf scale=w=842:h=480:force_original_aspect_ratio=decrease");
+            command.Append($" -c:a {transcodeProfile.AudioCodec ?? "copy"}");
+            command.Append(transcodeProfile.AudioSampleRate_480 > 0 ? $" -ar {transcodeProfile.AudioSampleRate_480}" : string.Empty);
+            command.Append($" -c:v {transcodeProfile.VideoCodec ?? "copy"}");
+            command.Append(!transcodeProfile.Preset.Equals("default") ? $" -preset {transcodeProfile.Preset}" : string.Empty);
+            command.Append($" -profile:v {transcodeProfile.VideoProfile ?? "main"}");
+            command.Append($" -crf {(transcodeProfile.CRF > 0 ? transcodeProfile.CRF.ToString() : "20")}");
+            command.Append(" -hls_time 4");
+            command.Append(" -hls_playlist_type LIST_TYPE");
+            command.Append(transcodeProfile.VideoBitrate_480 > 0 ? $" -b:v {transcodeProfile.VideoBitrate_480}k" : string.Empty);
+            command.Append(transcodeProfile.MaxBitrate_480 > 0 ? $" -maxrate {transcodeProfile.MaxBitrate_480}k" : string.Empty);
+            command.Append(transcodeProfile.BufferSize_480 > 0 ? $" -bufsize {transcodeProfile.BufferSize_480}k" : string.Empty);
+            command.Append(transcodeProfile.AudioBitrate_480 > 0 ? $" -b:a {transcodeProfile.AudioBitrate_480}k" : string.Empty);
+            command.Append(" -hls_segment_filename OUTPUT_SRC");
+
+            // 640x360
+            command.Append(" -vf scale=w=640:h=360:force_original_aspect_ratio=decrease");
+            command.Append($" -c:a {transcodeProfile.AudioCodec ?? "copy"}");
+            command.Append(transcodeProfile.AudioSampleRate_360 > 0 ? $" -ar {transcodeProfile.AudioSampleRate_480}" : string.Empty);
+            command.Append($" -c:v {transcodeProfile.VideoCodec ?? "copy"}");
+            command.Append(!transcodeProfile.Preset.Equals("default") ? $" -preset {transcodeProfile.Preset}" : string.Empty);
+            command.Append($" -profile:v {transcodeProfile.VideoProfile ?? "main"}");
+            command.Append($" -crf {(transcodeProfile.CRF > 0 ? transcodeProfile.CRF.ToString() : "20")}");
+            command.Append(" -hls_time 4");
+            command.Append(" -hls_playlist_type LIST_TYPE");
+            command.Append(transcodeProfile.VideoBitrate_360 > 0 ? $" -b:v {transcodeProfile.VideoBitrate_360}k" : string.Empty);
+            command.Append(transcodeProfile.MaxBitrate_360 > 0 ? $" -maxrate {transcodeProfile.MaxBitrate_360}k" : string.Empty);
+            command.Append(transcodeProfile.BufferSize_360 > 0 ? $" -bufsize {transcodeProfile.BufferSize_360}k" : string.Empty);
+            command.Append(transcodeProfile.AudioBitrate_360 > 0 ? $" -b:a {transcodeProfile.AudioBitrate_360}k" : string.Empty);
+            command.Append(" -hls_segment_filename OUTPUT_SRC");
 
             return command.ToString();
         }
