12f119ea8ea5747e105532b40ad6b2cd69619450
Ayman Aboelazm <ayman.m.aboelazm@gmail.com>
2020-08-14 15:32:43 +0200

    updates

--- Streamia/Utilies/AppClaimsPrincipalFactory.cs
+++ Streamia/Utilies/AppClaimsPrincipalFactory.cs
@@ -11,28 +11,32 @@ namespace Streamia.Utilies
 {
     public class AppClaimsPrincipalFactory : UserClaimsPrincipalFactory<AppUser, IdentityRole>
     {
-
-        public AppClaimsPrincipalFactory(
-            UserManager<AppUser> userManager
-            , RoleManager<IdentityRole> roleManager
-            , IOptions<IdentityOptions> optionsAccessor)
+        public AppClaimsPrincipalFactory
+        (
+            UserManager<AppUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IOptions<IdentityOptions> optionsAccessor
+        )
         : base(userManager, roleManager, optionsAccessor)
         { }
+
         public async override Task<ClaimsPrincipal> CreateAsync(AppUser user)
         {
             var principal = await base.CreateAsync(user);
 
             if (!string.IsNullOrWhiteSpace(user.Name))
             {
-                ((ClaimsIdentity)principal.Identity).AddClaims(new[] {
-                new Claim("Name", user.Name.ToString())
+                ((ClaimsIdentity) principal.Identity).AddClaims(new[] 
+                {
+                    new Claim("Name", user.Name.ToString())
                 });
             }
 
             if (!string.IsNullOrWhiteSpace(user.ProfilePicture))
             {
-                ((ClaimsIdentity)principal.Identity).AddClaims(new[] {
-                new Claim("ProfilePicture", user.ProfilePicture.ToString())
+                ((ClaimsIdentity)principal.Identity).AddClaims(new[] 
+                {
+                    new Claim("ProfilePicture", user.ProfilePicture.ToString())
                 });
             }
 
